6451ae81f2a991bfbac6360d675a3b3a87710a7b
sutin1234 <poly.dev2018@gmail.com>
2021-05-01 02:14:03 +0700

    fix container criteria|page

--- src/routes/design/criteria.svelte
+++ src/routes/design/criteria.svelte
@@ -6,7 +6,7 @@
 	<title>Design Criteria | RC Slab Design Online</title>
 </svelte:head>
 
-<section class="p-10">
+<section class="p-10 lg:p-60 xl:px-96 xl:pt-0 lg:pt-0">
 	<h2 class="text-center text-3xl">
 		<b class="text-red-500">Online Nonexpert Floor </b>
 		<p class="text-indigo-400 p-2">Construction System using Reinforced Concrete Slab</p>
